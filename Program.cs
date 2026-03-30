6e8589065434c1264e71b57c0b53e24e0ebcd188
adamsnode <adamsnode@gmail.com>
2026-03-30 18:39:45 +0200

    utworzenie projektu i pierwszy zarys

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+       
+
+int[] liczby = {1, 2, 3, 4};
+Console.WriteLine(liczby[0]);
+
+Struct[]
+
+foreach (int x in liczby)
+        {
+            Console.WriteLine(x);
+        }
+
+
+
+Student s1;
+s1.imie = "Adam";
+s1.wiek = 35;
+
+Student s2;
+s2.imie = "Piotr";
+s2.wiek = 17;
+
+Student s3;
+s3.imie = "Bolesław";
+s3.wiek = 100;
+
+
+
+}
+
+struct Student
+    {
+        public string imie;
+        public int wiek;
+    }
