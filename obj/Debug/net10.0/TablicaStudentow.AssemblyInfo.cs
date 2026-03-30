2a2d97b99a928155d3932b00e6216e257f4b5c60
adamsnode <adamsnode@gmail.com>
2026-03-30 19:36:43 +0200

    fix readme

--- obj/Debug/net10.0/TablicaStudentow.AssemblyInfo.cs
+++ obj/Debug/net10.0/TablicaStudentow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TablicaStudentow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41fb457530d849058a37ec1696714d4b21dcf91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2d07ae40fffe35dfbcf53215b9203e396b52bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TablicaStudentow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TablicaStudentow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
